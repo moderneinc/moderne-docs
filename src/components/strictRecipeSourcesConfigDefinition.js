82629e56ecbf84773d2075ca122817f2387f75b4
Mike Solomon <mike-solomon@users.noreply.github.com>
2025-04-23 10:21:24 -0700

    Actually add strict recipe sources vars

--- src/components/strictRecipeSourcesConfigDefinition.js
+++ src/components/strictRecipeSourcesConfigDefinition.js
@@ -7,31 +7,85 @@ const strictRecipeSourcesConfigDefinition = {
   label: 'Strict Recipe Sources Configuration',
   fields: [
     { 
-      label: 'Recipe Sources URL', 
-      key: 'recipeSourcesUrl', 
-      envKey: 'MODERNE_AGENT_RECIPE_SOURCES_SERVICE_URL',
-      description: 'The URL of your GraphQL service that provides recipe sources information.',
+      label: 'Use Only Configured Recipe Sources', 
+      key: 'useOnlyConfigured', 
+      envKey: 'MODERNE_AGENT_RECIPE_USEONLYCONFIGURED',
+      description: 'Only use the recipe sources configured in the agent.',
       required: true,
+      type: 'boolean',
+      defaultValue: ''
+    },
+    { 
+      label: 'POM Cache Type', 
+      key: 'pomCacheType', 
+      envKey: 'MODERNE_AGENT_RECIPE_POMCACHE_TYPE',
+      description: 'Used to specify what type of cache the POM should use. Acceptable values: <strong>IN_MEMORY</strong> or <strong>REDIS</strong>.',
+      required: false,
       type: 'text',
       defaultValue: ''
     },
     { 
-      label: 'Update Interval Seconds', 
-      key: 'updateInterval', 
-      envKey: 'MODERNE_AGENT_RECIPE_SOURCES_UPDATE_INTERVAL_SECONDS',
-      description: 'Specifies how often to request your recipe sources information.',
+      label: 'Entry TTL Minutes', 
+      key: 'entryTtlMinutes', 
+      envKey: 'MODERNE_AGENT_RECIPE_POMCACHE_ENTRYTTLMINUTES',
+      description: 'How long entries should live in the POM cache.',
       required: false,
       type: 'text',
-      defaultValue: '600'
+      defaultValue: ''
     },
     { 
-      label: 'Skip SSL Verification', 
-      key: 'skipSSL', 
-      envKey: 'MODERNE_AGENT_RECIPE_SOURCES_SKIPSSL',
-      description: 'Specifies whether or not to skip SSL validation for HTTP connections to this Recipe Sources service instance. This must be set to true if you use a self-signed SSL/TLS certificate.',
+      label: 'Redis Host', 
+      key: 'redisHost', 
+      envKey: 'MODERNE_AGENT_RECIPE_POMCACHE_REDIS_HOST',
+      description: 'The URL of the Redis instance. <strong>This must be set if POM Cache Type is set to REDIS.</strong>',
+      required: false,
+      type: 'text',
+      defaultValue: ''
+    },
+    { 
+      label: 'Redis Port', 
+      key: 'redisPort', 
+      envKey: 'MODERNE_AGENT_RECIPE_POMCACHE_REDIS_PORT',
+      description: 'The port number of the Redis instance. <strong>This must be set if POM Cache Type is set to REDIS.</strong>',
+      required: false,
+      type: 'text',
+      defaultValue: ''
+    },
+    { 
+      label: 'Redis Username', 
+      key: 'redisUsername', 
+      envKey: 'MODERNE_AGENT_RECIPE_POMCACHE_REDIS_USERNAME',
+      description: 'The username needed to authenticate to the Redis instance.',
+      required: false,
+      type: 'text',
+      defaultValue: ''
+    },
+    { 
+      label: 'Redis Password', 
+      key: 'redisPassword', 
+      envKey: 'MODERNE_AGENT_RECIPE_POMCACHE_REDIS_PASSWORD',
+      description: 'The password needed to authenticate with the Redis instance.',
+      required: false,
+      type: 'text',
+      defaultValue: ''
+    },
+    { 
+      label: 'Redis SSL', 
+      key: 'redisSsl', 
+      envKey: 'MODERNE_AGENT_RECIPE_POMCACHE_REDIS_SSL',
+      description: 'If set to true, then SSL will be enabled for the connection to the Redis instance.',
       required: false,
       type: 'boolean',
       defaultValue: 'false'
+    },
+    { 
+      label: 'Redis DB Index', 
+      key: 'redisDBIndex', 
+      envKey: 'MODERNE_AGENT_RECIPE_POMCACHE_REDIS_DATABASE',
+      description: 'The Redis DB index.',
+      required: false,
+      type: 'text',
+      defaultValue: ''
     },
   ]
 };
